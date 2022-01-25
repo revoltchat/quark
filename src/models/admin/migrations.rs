a4730baee460d45af5cd1d01d0b75a105b930f5d
Paul Makles <paulmakles@gmail.com>
2022-01-25 11:51:31 +0000

    chore(doc): document migrations

--- src/models/admin/migrations.rs
+++ src/models/admin/migrations.rs
@@ -1,7 +1,11 @@
 use serde::{Deserialize, Serialize};
 
+/// Document representing migration information
 #[derive(Serialize, Deserialize)]
 pub struct MigrationInfo {
-    _id: i32,
+    /// Unique Id
+    #[serde(rename = "_id")]
+    id: i32,
+    /// Current database revision
     revision: i32,
 }
